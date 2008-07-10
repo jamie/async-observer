4e34b3be8dc00fc2635c68d29ec1029ffa97a946
Jamie Macey <jamie@almlabs.com>
2008-07-10 10:52:49 -0400

    use define_method instead of eval so that the method shows up where we are expecting it

--- lib/async_observer/data_mapper.rb
+++ lib/async_observer/data_mapper.rb
@@ -12,21 +12,22 @@ module DataMapper::Resource::ClassMethods
     @async_hooks[seq] = block
     ahook = :"_async_after_#{target_method}_#{seq}"
 
-    # DataMapper hooks don't take that extra param, the current object
-    # is just supposed to be available in self.
-    code = "def #{ahook}; @async_hooks[#{seq}].call; end"
-    instance_eval(code, __FILE__, __LINE__ - 1)
+    define_method ahook do
+      @async_hooks[seq].call()
+    end
 
     after target_method do
       async_send(ahook)
     end
   end
 
+  # untested
   def send_to_instance(id, selector, *args)
     x = get(id)
     x.send(selector, *args) if x
   end
 
+  # untested
   def async_each_opts(selector, opts, *args)
     min = opts.fetch(:min, minimum(:id))
     max = opts.fetch(:max, maximum(:id))
@@ -34,6 +35,7 @@ module DataMapper::Resource::ClassMethods
     (min..max).async_each_opts(self, :send_to_instance, opts, selector, *args)
   end
 
+  # untested
   def async_each(selector, *args)
     async_each_opts(selector, {}, *args)
   end
